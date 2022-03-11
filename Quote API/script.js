02aef49d85326e6b120431776e0be9fc2ea348bb
jocybo <naveencybosec@gmail.com>
2022-03-11 23:58:12 +0530

    API

--- Quote API/script.js
+++ Quote API/script.js
@@ -10,22 +10,3 @@ function output() {
             document.getElementById('append').innerHTML = `<p id="t-color"class="text-center fs-3">${res.quote}</p>`;
         })
 }
-
-// MULTIPLE QUOTE :
-
-/* function output() {
-    let value = document.getElementById('num').value;
-    fetch(`https://the-dune-api.herokuapp.com/quotes/${value}`)
-        .then((data) => data.json())
-        .then((res) => {
-            res.map((op) => {
-
-                let display = document.createElement('p');
-                display.innerHTML = `${op.quote}`;
-                document.getElementById('quote').appendChild(display)
-                console.log(res);
-            })
-
-
-        })
-} */
